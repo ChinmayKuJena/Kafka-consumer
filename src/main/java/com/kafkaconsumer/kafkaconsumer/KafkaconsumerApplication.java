4f487cf83ecef6122b7b44123b5c249850c2f537
ChinmayKuJena <chinmay.jena7878@gmail.com>
2024-06-24 19:36:34 +0530

    some log added and running command!

--- src/main/java/com/kafkaconsumer/kafkaconsumer/KafkaconsumerApplication.java
+++ src/main/java/com/kafkaconsumer/kafkaconsumer/KafkaconsumerApplication.java
@@ -1,13 +1,18 @@
 package com.kafkaconsumer.kafkaconsumer;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaconsumerApplication {
+	static org.slf4j.Logger log = LoggerFactory.getLogger(KafkaconsumerApplication.class);
+
 	// later add config datas
 	public static void main(String[] args) {
+		log.info("http://localhost:9999/kafka/consume/abc");
 		SpringApplication.run(KafkaconsumerApplication.class, args);
+
 	}
 
 }
